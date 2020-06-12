e1f6a30fd85dcea3e100f0d2f05656bf826af02d
ZHENGQING ZHU <zhuzhengqing321@qq.com>
2020-06-12 23:28:59 +0800

    no message

---
+++ 01-介绍/01-helloworld.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# 在Python2中, 如果文件内出现中文(不论是注释内, 还是代码内), 必须在文件的开头用注释的方式声明文件的编码方式
+# 在Python3中, 不需要
+
+"""
+hello world
+"""
+
+print("hello world")
+print("你好, 世界")
---
+++ 01-介绍/05-available_types.py
@@ -0,0 +1,12 @@
+"""
+基本数据类型
+"""
+
+a = 1
+print(type(a))  # int
+
+a = "1"
+print(type(a))  # str
+
+a = 1.1
+print(type(a))  # float
