75cb6887e4b3af387a6e395045b6270621826a2a
zzq5271137 <zzq5271137@yeah.net>
2020-07-01 18:20:55 +0800

    no message

--- 12-模块和包/my_package/my_request.py
+++ 12-模块和包/my_package/my_request.py
@@ -1,4 +1,10 @@
 def get_request(url):
     data = {'body': 'abc'}
-    print('您的请求%s已经下载下来了, 数据为: %s' % (url, data))
+    print('您的GET请求%s已经下载下来了, 数据为: %s' % (url, data))
+    return data
+
+
+def post_request(url):
+    data = {'body': 'abc'}
+    print('您的POST请求%s已经下载下来了, 数据为: %s' % (url, data))
     return data
