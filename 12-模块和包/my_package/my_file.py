378ebf72ca6e0f10e0b0ea4ea077c206a0bd5c6b
zzq5271137 <zzq5271137@yeah.net>
2020-07-01 18:17:41 +0800

    no message

---
+++ 12-模块和包/my_package/my_file.py
@@ -0,0 +1,2 @@
+def save_data(data):
+    print('数据%s已经保存' % data)
---
+++ 12-模块和包/my_package/my_request.py
@@ -0,0 +1,4 @@
+def get_request(url):
+    data = {'body': 'abc'}
+    print('您的请求%s已经下载下来了, 数据为: %s' % (url, data))
+    return data
