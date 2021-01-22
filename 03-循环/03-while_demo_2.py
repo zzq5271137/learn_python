e7df6f2760c55f0815a64541e3f9034e14f17bf6
zhengqing.zhu <zhengqing.zhu@tcl.com>
2021-01-22 09:25:24 +0800

    no message

--- 03-循环/03-while_demo_2.py
+++ 03-循环/03-while_demo_2.py
@@ -3,10 +3,17 @@
 """
 
 target = 37
-input_value = int(input("请输入数字: "))
-while input_value != target:
-    if input_value < target:
-        input_value = int(input("输入的数字小了, 请再次输入: "))
-    elif input_value > target:
-        input_value = int(input("输入的数字大了, 请再次输入: "))
-print("正确！")
+
+while True:
+    try:
+        input_value = int(input("enter: "))
+        if input_value == target:
+            print("bingo!")
+            break
+        if input_value < target:
+            print("need be larger")
+        if input_value > target:
+            print("need be smaller")
+    except Exception as e:
+        print("wrong input, try again")
+        continue
