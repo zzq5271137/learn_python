8cc77ac2dc250b0de0cd1063459f616fbdb34f8b
zhengqing.zhu <zhengqing.zhu@tcl.com>
2021-02-21 18:43:21 +0800

    no message

--- 10-面向对象/16-object_singleton.py
+++ 10-面向对象/16-object_singleton.py
@@ -13,14 +13,19 @@ class Calculator(object):
     # 这种写法的单例模式是线程不安全的; 如果想要线程安全的单例模式, 需要使用锁(之后会讲到);
     def __new__(cls, *args, **kwargs):  # 通过__new__()方法来实现单例模型
         if not cls.__instance:
-            cls.__instance = super().__new__(cls)
+            # cls.__instance = super().__new__(cls)  # 简写
+            cls.__instance = super(Calculator, cls).__new__(cls)
         return cls.__instance
 
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
 
-c1 = Calculator('c1')
-print("id: {}, name: {}".format(id(c1), c1.name))  # id()函数返回对象的唯一标识符, 标识符是一个整数
-c2 = Calculator('c2')
-print("id: {}, name: {}".format(id(c2), c2.name))
+c1 = Calculator(name='c1', age=101)
+print("create c1: Calculator(name='c1', age=101)")
+print(f"c1: id={id(c1)}, name={c1.name}, age={c1.age}")  # id()函数返回对象的唯一标识符, 标识符是一个整数
+c2 = Calculator(name='c2', age=102)
+print("create c2: Calculator(name='c2', age=102)")
+print(f"c2: id={id(c2)}, name={c2.name}, age={c2.age}")
+print(f"c1: id={id(c1)}, name={c1.name}, age={c1.age}")
